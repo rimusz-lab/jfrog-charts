696e406d630ffc4e76f6180dcf1df15aac84123f
rimas <rmocius@gmail.com>
2018-09-26 14:06:21 +0100

    no message

--- test/e2e-minikube.sh
+++ test/e2e-minikube.sh
@@ -7,12 +7,13 @@ set -o pipefail
 readonly REPO_ROOT="${REPO_ROOT:-$(git rev-parse --show-toplevel)}"
 
 run_minikube() {
-    # Download kubectl, which is a requirement for using minikube.
+    echo "Download kubectl, which is a requirement for using minikube..."
     curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/${K8S_VERSION}/bin/linux/amd64/kubectl && chmod +x kubectl && sudo mv kubectl /usr/local/bin/
 
-    # Download minikube.
+    echo "Download minikube..."
     curl -Lo minikube https://github.com/kubernetes/minikube/releases/download/${MINIKUBE_VERSION}/minikube-linux-amd64 && chmod +x minikube && sudo mv minikube /usr/local/bin/
 
+    echo "Setup Minikuke..."
     # TODO: remove the --bootstrapper flag once this issue is solved: https://github.com/kubernetes/minikube/issues/2704
     sudo minikube config set WantReportErrorPrompt false
     sudo -E minikube start --cpus 2 --memory 6144 --vm-driver=none --bootstrapper=localkube --kubernetes-version=${K8S_VERSION} --extra-config=apiserver.Authorization.Mode=RBAC
@@ -20,17 +21,18 @@ run_minikube() {
     # Fix the kubectl context, as it's often stale.
     minikube update-context
 
-    # Wait for Kubernetes to be up and ready.
+    echo "Wait for Kubernetes to be up and ready..."
     JSONPATH='{range .items[*]}{@.metadata.name}:{range @.status.conditions[*]}{@.type}={@.status};{end}{end}'; until kubectl get nodes -o jsonpath="$JSONPATH" 2>&1 | grep -q "Ready=True"; do sleep 1; done
 
-    # Get cluster info
+    echo "Get cluster info"
     kubectl cluster-info
-    # Create cluster admin
+    echo "Create cluster admin"
     kubectl create clusterrolebinding add-on-cluster-admin --clusterrole=cluster-admin --serviceaccount=kube-system:default
 }
 
 run_tillerless() {
      # -- Work around for Tillerless Helm, till Helm v3 gets released -- #
+     echo "Install Tillerless Helm plugin..."
      # shellcheck disable=SC2154
      docker exec "$config_container_id" helm init --client-only
      # shellcheck disable=SC2154
@@ -44,7 +46,7 @@ run_tillerless() {
 
 main() {
 
-    # Start Minikube
+    echo "Start Minikube...""
     run_minikube
 
     git remote add k8s "${CHARTS_REPO}" &> /dev/null || true
