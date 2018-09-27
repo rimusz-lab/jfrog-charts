c83595b0b7b510c9bdad34bec638b2b055aa2f4a
rimas <rmocius@gmail.com>
2018-09-27 10:37:38 +0100

    no message

--- test/e2e-github.sh
+++ test/e2e-github.sh
@@ -1,17 +1,16 @@
 #!/usr/bin/env bash
 
 set -o errexit
-set -o nounset
 set -o pipefail
 
 main() {
 
     if [ -n "${GCLOUD_GKE_CLUSTER:-}" ]; then
-      echo "Running from branch in GKE..."
-      echo
-      test/e2e-gke.sh
+        echo "PR is from main repo, running in GKE..."
+        echo
+        test/e2e-gke.sh
     else
-        echo "Running from fork in Minikube..."
+        echo "PR is from fork, running in Minikube..."
         echo
         test/e2e-minikube.sh
     fi
