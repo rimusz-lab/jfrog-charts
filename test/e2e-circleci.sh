635220ee8bd1fd8853a61df8ace90f5690c3bfc7
rimas <rmocius@gmail.com>
2018-09-27 08:16:53 +0100

    no message

--- test/e2e-circleci.sh
+++ test/e2e-circleci.sh
@@ -7,10 +7,12 @@ set -o pipefail
 main() {
 
     if [ -z "$GCLOUD_GKE_CLUSTER" ]; then
-        echo "Running from fork..."
+        echo "Running from fork in Minikube..."
+        echo
         test/e2e-minikube.sh
     else
-        echo "Running from branch..."
+        echo "Running from branch in GKE..."
+        echo
         test/e2e-gke.sh
     fi
 
